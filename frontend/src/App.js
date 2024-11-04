bc8ceb0bf61845a8947436553ce0a12eebc531fb
Imperius5 <muhammedthufayl@gmail.com>
2024-11-04 21:53:22 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Collection from '.pages/Collection'
-import Cart from '.pages/Cart'
-import Contact from '.pages/Contact'
-import About from '.pages/About'
-import Login from '.pages/Login'
-import Orders from '.pages/Orders'
-import PlaceOrder from '.pages/PlaceOrder'
-import Product from '.pages/Product'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[3vw] md:px-[5vw] lg:px-[2vw]'>
+    <Navbar />
     <Routes>
       <Route path = '/' element = {<Home/>}/>
       <Route path = '/collection' element = {<Collection/>} />
@@ -24,6 +26,7 @@ const App = () => {
       <Route path = '/place-order' element = {<PlaceOrder/>} />
       <Route path = '/orders' element = {<Orders/>} />
     </Routes>
+    </div>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import logo from "../assets/logo-placeholder.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className = "flex items-center justify-between py-5 font-medium" >
+      <img src={logo} className = "w-36" alt="" />
+    </div>
   )
 }
 
