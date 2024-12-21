e432c0ab095c32675969ff1a4eeb716a2c51ca0a
Imperius5 <muhammedthufayl@gmail.com>
2024-12-21 13:21:39 +0530

    Login and orders page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import logo from "../assets/logo.png"
 import search from "../assets/search_icon.png"
-import profileIcon from "../assets/profile.png"
 import cartIcon from "../assets/cart_icon.png"
 import menuIcon from "../assets/menu_icon.png"
 import backIcon from "../assets/back_icon.png"
@@ -36,7 +35,7 @@ const Navbar = () => {
     <div className='flex items-center gap-6'>
       <img onClick ={() => setShowSearch(true)}src = {search} alt = "search" className='w-5 cursor-pointer'/>
       <div className='group relative'>
-        <img src = {profileIcon}  alt = "profile" className='w-5 cursor-pointer'/>
+        <Link to='/login'><img src = {assets.profile_icon}  alt = "profile" className='w-5 cursor-pointer'/></Link>
         <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
           <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
             <p className='cursor-pointer hover:text-black'>Profile</p>
