2bb512daccaa30c0ef7dba7c3cb30dcba097ffdb
Imperius5 <muhammedthufayl@gmail.com>
2025-01-02 19:46:30 +0530

    deleted unused pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
@@ -25,8 +23,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
